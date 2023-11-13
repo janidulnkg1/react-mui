ebffaba5bb3c2c1a80837a0e609b30bd2757d18d
janidulnkg1 <janidulnkg1@gmail.com>
2023-11-13 16:58:01 +0530

    Add MuiTabs

---
+++ .history/mui-demo/src/App_20231113165218.tsx
@@ -0,0 +1,78 @@
+import './App.css';
+//import { MuiTypography } from './components/MuiTypography';
+//import { MuiButton } from './components/MuiButton';
+//import {MuiTextField } from './components/MuiTextField'
+//import {MuiSelect } from './components/MuiSelect'
+//import { MuiRadioButton } from './components/MuiRadioButton';
+//import { MuiCheckbox } from './components/MuiCheckbox';
+//import { MuiSwitch } from './components/MuiSwitch';
+//import { MuiRating } from './components/MuiRating';
+//import { MuiAutocomplete } from './components/MuiAutocomplete';
+//import { MuiLayout } from './components/MuiLayout';
+//import { MuiCard } from './components/MuiCard';
+//import { MuiAccordion } from './components/MuiAccordion';
+//import { MuiLink} from './components/MuiLink';
+//import { MuiBreadcrumbs } from './components/MuiBreadcrumbs';
+//import { MuiImageList } from './components/MuiImageList';
+//import { MuiAvatar } from './components/MuiAvatar';
+//import { MuiImageList } from './components/MuiImageList';
+//import { MuiBadge } from './components/MuiBadge';
+//import { MuiList } from './components/MuiList';
+//import { MuiChip } from './components/MuiChip';
+//import { MuiTooltip } from './components/MuiTooltip';
+//import { MuiTable } from './components/MuiTable';
+//import { MuiAlert } from './components/MuiAlert';
+//import { MuiSnackbar } from './components/MuiSnackbar';
+//import { MuiDialog } from './components/MuiDialog';
+//import { MuiProgress } from './components/MuiProgress';
+//import { MuiSkeleton } from './components/MuiSkeleton';
+//import { MuiLoadingButton } from './components/MuiLoadingButton';
+
+import { MuiSpeedDial } from './components/MuiSpeedDial';
+import { MuiNavbar } from './components/MuiNavbar';
+import { MuiDrawer } from './components/MuiDrawer';
+import { MuiBottomNavigation } from './components/MuiBottomNavigation';
+import { MuiDateTimePicker } from './components/MuiDateTimePicker';
+
+function App() {
+  return (
+    <div className="App">
+     
+      {/*MuiTypography */}
+      {/*MuiButton */}
+      {/*MuiTextField */}
+      {/*MuiSelect */}
+      {/*MuiRadioButton */}
+      {/*MuiCheckbox*/}
+      {/*MuiSwitch */}
+      {/*MuiRating */}
+      {/*MuiAutocomplete */}
+      {/*MuiLayout */}
+      {/*MuiCard */}
+      {/*MuiAccordion */}
+      {/*MuiImageList */}
+      {/*MuiLink */}
+      {/*MuiBreadcrumbs */}
+      {/*MuiAvatar */}
+      {/*MuiBadge */}
+      {/*MuiList */}
+      {/*MuiTooltip */}
+      {/*MuiTable */}
+      {/*MuiAlert */}
+      {/*MuiSnackbar */}
+      {/*MuiDialog */}
+      {/*MuiProgress */}
+      {/*MuiSkeleton */}
+      {/*MuiLoadingButton */}
+      
+
+      <MuiNavbar />
+      <MuiDrawer />
+      <MuiDateTimePicker />
+      <MuiSpeedDial /> 
+      <MuiBottomNavigation />
+    </div>
+  );
+}
+
+export default App;
